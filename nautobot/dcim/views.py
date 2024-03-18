a76ffb6b8845e8ccc3685d9c5f5aa739ca7fff65
Xander Harris <xandertheharris@gmail.com>
2024-03-17 21:49:43 -0700

    replace ordereddict instances
    See #10
    
    @edwardtheharris - xandertheharris@gmail.com
    
    Xander Harris
    
    Changelog: changed

--- nautobot/dcim/views.py
+++ nautobot/dcim/views.py
@@ -1,4 +1,3 @@
-from collections import OrderedDict
 import uuid
 
 from django.contrib import messages
@@ -700,18 +699,16 @@ class DeviceTypeImportView(generic.ObjectImportView):
     ]
     queryset = DeviceType.objects.all()
     model_form = forms.DeviceTypeImportForm
-    related_object_forms = OrderedDict(
-        (
-            ("console-ports", forms.ConsolePortTemplateImportForm),
-            ("console-server-ports", forms.ConsoleServerPortTemplateImportForm),
-            ("power-ports", forms.PowerPortTemplateImportForm),
-            ("power-outlets", forms.PowerOutletTemplateImportForm),
-            ("interfaces", forms.InterfaceTemplateImportForm),
-            ("rear-ports", forms.RearPortTemplateImportForm),
-            ("front-ports", forms.FrontPortTemplateImportForm),
-            ("device-bays", forms.DeviceBayTemplateImportForm),
-        )
-    )
+    related_object_forms = {
+        "console-ports": forms.ConsolePortTemplateImportForm,
+        "console-server-ports": forms.ConsoleServerPortTemplateImportForm,
+        "power-ports": forms.PowerPortTemplateImportForm,
+        "power-outlets": forms.PowerOutletTemplateImportForm,
+        "interfaces": forms.InterfaceTemplateImportForm,
+        "rear-ports": forms.RearPortTemplateImportForm,
+        "front-ports": forms.FrontPortTemplateImportForm,
+        "device-bays": forms.DeviceBayTemplateImportForm,
+    }
 
 
 class DeviceTypeBulkEditView(generic.BulkEditView):
